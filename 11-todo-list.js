779c89ce62c4d96afac7853752288644fae4c0ec
DevupriyaMS <devupriya.manuranisureshkumar@student.griffith.ie>
2023-11-23 16:28:23 +0000

    to do list complete

--- 11-todo-list.js
+++ 11-todo-list.js
@@ -1,16 +1,54 @@
-const todoarray = [];
+const todoList = [{
+    name: 'make dinner',
+    dueDate: '2022-12-22'
+}, {
+    name: 'wash dishes',
+    dueDate: '2022-12-22'
+}];
 
-function addtodo() {
-    const inputElement = document.querySelector('.js-input');
+renderTodoList();
+
+function renderTodoList() {
+    let todoListHTML = '';
+
+    for (let i = 0; i < todoList.length; i++) {
+        const todoObject = todoList[i];
+        //const name = todoObject.name;
+        //const dueDate = todoObject.dueDate;
+        const {
+            name,
+            dueDate
+        } = todoObject;
+        const html = `
+        <div>${name}</div>
+        <div>${dueDate}</div>
+        <button onclick="
+          todoList.splice(${i}, 1);
+          renderTodoList();
+        " class="delete-todo-button">Delete</button> 
+      `;
+        todoListHTML += html;
+    }
+
+    document.querySelector('.js-todo-list')
+        .innerHTML = todoListHTML;
+}
+
+function addTodo() {
+    const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
 
-    todoarray.push(name);
-    console.log(todoarray)
+    const dateInputElement = document.querySelector('.js-due-date-input');
+    const dueDate = dateInputElement.value;
+
+    todoList.push({
+        //name: name,
+        //dueDate: dueDate,
+        name,
+        dueDate
+    });
+
+    inputElement.value = '';
+
+    renderTodoList();
 }
-
-
-for (let i = 0; i < todoarray.length; i++) {
-    console.log(todoarray[i])
-}
-
-document.querySelector(".js-todo-list").innerHTML = todoarray
